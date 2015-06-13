ac95768ff6c3e159ea415a5e3409e75f3843d8c6
chenso <chen.s@wustl.edu>
2015-06-13 13:55:46 -0400

    Update publish_hex.sh

--- scripts/ci/publish_hex.sh
+++ scripts/ci/publish_hex.sh
@@ -1,11 +1,3 @@
 cd /$HOME/$CIRCLE_PROJECT_REPONAME/deps/ex_doc
 mix do deps.get, deps.compile, compile
 mix docs
-
-mix hex.user auth
-expect "Username:*"
-send -- "$HEX_USERNAME\r"
-expect "Password:*"
-send -- "$HEX_PASSWORD\r"
-
-mix hex.publish
